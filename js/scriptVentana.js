c13b1f861757386599aa672802c5467e804cb353
josevillacres <nuna.josepajaro99@gmail.com>
2020-06-05 22:46:49 -0500

    Login001

---
+++ js/scriptVentana.js
@@ -0,0 +1,7 @@
+function abrirFormulario(){
+    document.getElementById("vent").style.display="block";
+}
+
+function cerrarFormulario(){
+    document.getElementById("vent").style.display="none";
+}
